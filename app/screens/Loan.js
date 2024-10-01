0b08d34ff1a07895e04cf6b844be18e60749af16
Nishant0121 <142099904+Nishant0121@users.noreply.github.com>
2024-10-01 07:57:26 +0530

    loan

--- app/screens/Loan.js
+++ app/screens/Loan.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import {
   View,
   Text,
@@ -7,17 +7,45 @@ import {
   StyleSheet,
 } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { UserContext } from "../App";
+
+const genAI = new GoogleGenerativeAI("AIzaSyD66npWWEDp8zXmnI2X9FMPQwDQs6A4NIs");
 
 const Loan = () => {
   const [loanAmount, setLoanAmount] = useState("");
   const [interestRate, setInterestRate] = useState("");
   const [loanTerm, setLoanTerm] = useState("");
   const [monthlyPayment, setMonthlyPayment] = useState(null);
+  const { userData, setUserData } = useContext(UserContext);
+  const [result, setResult] = useState("");
 
-  const calculateLoan = () => {
+  const calculateLoan = async () => {
     const principal = parseFloat(loanAmount);
     const rate = parseFloat(interestRate) / 100 / 12;
     const term = parseFloat(loanTerm) * 12;
+
+    const model = genAI.getGenerativeModel({
+      model: "gemini-1.5-flash",
+    });
+
+    const geminiResult = await model.generateContent([
+      {
+        text: `User takes a loan with the following details: Principal: ₹
+          ${principal} 
+         Interest Rate: 
+          ${rate} 
+          %, Loan Term: 
+          term 
+           ${loanTerm}.
+          The user's salary is 
+          ₹${userData.salary} 
+           and the user's savings is 
+          ₹${userData.savings}
+          interest calculation method should be monthly compunded interest , loan frequency should be monthly
+          Give me a Single Line output nothing else output should be precise and on point response like 'If You increase montly installment By ₹ X you can reduce your loan term by Y months considering savings of ₹Z amount and ₹A salary ' `,
+      },
+    ]);
 
     if (principal > 0 && rate > 0 && term > 0) {
       const payment =
@@ -25,6 +53,8 @@ const Loan = () => {
         (Math.pow(1 + rate, term) - 1);
       setMonthlyPayment(payment.toFixed(2));
     }
+    console.log(geminiResult.response.text());
+    setResult(geminiResult.response.text());
   };
 
   return (
@@ -58,9 +88,16 @@ const Loan = () => {
         {monthlyPayment && (
           <View style={styles.resultContainer}>
             <Text style={styles.resultText}>Monthly Payment:</Text>
-            <Text style={styles.resultAmount}>${monthlyPayment}</Text>
+            <Text style={styles.resultAmount}>₹{monthlyPayment}</Text>
           </View>
         )}
+        <View>
+          {result && (
+            <View style={styles.resultContainer}>
+              <Text style={styles.resultText}>{result}</Text>
+            </View>
+          )}
+        </View>
       </View>
     </LinearGradient>
   );
